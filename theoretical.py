f2cf65398c9dae199f64209945d890f864c90d9b
IT <NadiaIT@Nadias-MBP.fritz.box>
2024-07-19 12:35:33 +0200

    theori... added to identify the types of distributions and outliers more effectively

---
+++ theoretical.py
@@ -0,0 +1,34 @@
+# Import mathematical and statistical libraries
+import numpy as np
+from scipy import stats
+from IPython.display import display
+import pandas as pd
+import matplotlib.pyplot as plt
+from main import data_a
+
+# Column selection
+column = data_a['AOV']  
+
+# Calculate the theoretical distributions based on the data
+x_values = np.linspace(min(column), max(column), 100)  # Create a range of values for the x-axis
+#(it creates 100 points between the minimum and maximum values of the 'AOV' column from my dataset.)
+
+distributions = {  # Create a dictionary with calculated theoretical distributions
+    'Normal': stats.norm.pdf(x_values, *stats.norm.fit(column)), #pdf: probability density function (PDF)
+    'Exponential': stats.expon.pdf(x_values, *stats.expon.fit(column)),
+    'Uniform': stats.uniform.pdf(x_values, loc=min(column), scale=max(column)-min(column))
+}
+
+# Specify and extract distribution to use
+distribution_name = 'Exponential'  
+distribution = distributions[distribution_name] 
+
+# Plotting
+fig, axes = plt.subplots(1, 1, figsize=(16, 5))  # Create a single plot
+axes.hist(column, bins=70, alpha=0.5, label=column.name, density=True)  # Histogram of the data
+axes.plot(x_values, distribution, label='Theoretical Distribution')  # Theoretical distribution line
+axes.set_title(f'{distribution_name} Distribution')
+axes.set_xlabel(column.name)
+axes.set_ylabel('Density')
+axes.legend()  # Display legend
+plt.show()
