c17ff0d7b82fb60d37b2358333dc014555d7be79
IT <NadiaIT@Nadias-MBP.fritz.box>
2024-07-22 09:31:17 +0200

    determine type of all ditri..

---
+++ total_amount_spend_distribution.py
@@ -0,0 +1,29 @@
+
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+
+from actual_plot import data_a
+
+
+column = data_a['Total_Amount_Spent']
+x_values = np.linspace(min(column), max(column), 100)
+
+distributions = {
+    'Normal': stats.norm.pdf(x_values, *stats.norm.fit(column)),
+    'Exponential': stats.expon.pdf(x_values, *stats.expon.fit(column)),
+    'Lognormal': stats.lognorm.pdf(x_values, *stats.lognorm.fit(column))
+}
+
+distribution_name ='Lognormal'
+distribution = distributions[distribution_name]
+
+
+
+fig, axes = plt.subplots(1, 1, figsize=(16,5))
+axes.hist(column, bins= 70, alpha=0.5, label= column.name, density=True) #plo
+axes.plot(x_values, distribution, label='Theoretical Distibution')
+axes.set_xlabel(column.name)
+axes.set_ylabel('Density')
+axes.legend() 
+plt.show()
